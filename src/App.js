949deec06bf3998b8f4248d4ecc89747389c8c8f
guisantosalves <workguisantos@gmail.com>
2022-03-03 21:48:25 -0400

    Task-Tracker-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = 'guizaodozazapazp';
+  const x = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello react</h1>
+      <h2>Teste de sexo para {name} : {x ? 'yes' : 'no'}</h2>
     </div>
   );
 }
